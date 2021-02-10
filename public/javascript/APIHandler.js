4ea4510b0f6a5b28e0b992f4be85d2fee1d13009
Abel Andrés <abelandres.gtc@gmail.com>
2021-02-10 09:18:17 +0100

    not finished

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,21 +1,30 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
-  }
+    // baseUrl: 'https://minions-api.herokuapp.com/'
 
+    this.app = axios.create({
+    baseURL: 'https://minions-api.herokuapp.com/'
+    })
+  }
+  
   getFullList () {
+    return this.app.get('/characters')
 
   }
 
-  getOneRegister () {
+  getOneRegister (id) {
+    return this.app.get(`/characters/${id}`)
 
   }
 
   createOneRegister () {
+    return this.app.get('/characters', character)
 
   }
 
   updateOneRegister () {
+    return this.app.get(`/characters/${id}`, character)
 
   }
 
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,12 +2,43 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    // console.log(charactersAPI.getFullList())
+    charactersAPI
+      .getFullList()
+      .then(response => {
+        console.log(response)
+        // let characters = response.data.reverse(), list = ''
+        let charactersArr = response.data.reverse(), list = ''
+        charactersArr.forEach(elm => {
+        // console.log(elm.name)
+          list += `<li><p><strong>${elm.name}</strong> (Id ${elm.id})</p><p>Trabajo: ${elm.occupation}</p><p>Arma: ${elm.weapon}</p></li>`
+          document.querySelector('div.character-info').innerHTML = list
+        })
+      })
+      .catch(err => console.log('ERROR', err))
 
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    const characterId = document.querySelector('.character-id').value
+    // // console.log(characterId)
+    charactersAPI
+      .getOneRegister(characterId)
+      .then(response => fillEditForm(response.data))
+      .catch(err => console.log('ERROR', err))
   });
+
+  function fillEditForm(data) {
+
+    const inputs = document.querySelectorAll('#edit-character-form input')
+    console.log(data._id)
+
+    inputs[0].value = data._id
+    inputs[1].value = data.name
+    inputs[2].value = data.occupation
+    inputs[3].value = data.weapon
+    inputs[4].value = data.cartoon
+}
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
 
