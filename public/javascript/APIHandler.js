f049182b574ca7f058b5fec7e78f5a7bdbdc8b0a
Mario Ortiz <ortiz.mario22@gmail.com>
2021-04-20 19:22:12 +0200

    very fun exercise indeed

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,69 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseURL) {
+    this.BASE_URL = baseURL;
+    this.api = axios.create({ baseURL })
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.api.get('/characters')
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
+  getOneRegister(id) {
+    return this.api.get(`/characters/${id}`)
 
   }
 
-  deleteOneRegister () {
+  createOneRegister(characterInfo) {
+    return this.api.post('/characters', characterInfo)
 
   }
+
+  updateOneRegister(id, characterInfo) {
+    return this.api.put(`/characters/${id}`, characterInfo)
+  }
+
+  deleteOneRegister(id) {
+    return this.api.delete(`/characters/${id}`)
+  }
+
+  createHtmlBlock(elm) {
+    let currentDiv = document.querySelector(".characters-container");
+
+    let newCharacter = document.createElement("div")
+    let newId = document.createElement("div")
+    let newName = document.createElement("div")
+    let newOccupation = document.createElement("div")
+    let newCartoon = document.createElement("div")
+
+    newCharacter.className = "character-info"
+    newId.className = "name"
+    newName.className = "name"
+    newOccupation.className = "occupation"
+    newCartoon.className = "cartoon"
+
+    newId.innerHTML += "Id: " + elm.id
+    newName.innerHTML += "Name: " + elm.name
+    newOccupation.innerHTML += "Occupation: " + elm.occupation
+    newCartoon.innerHTML += "Cartoon: " + elm.cartoon
+
+    newCharacter.appendChild(newId)
+    newCharacter.appendChild(newName)
+    newCharacter.appendChild(newOccupation)
+    newCharacter.appendChild(newCartoon)
+
+    currentDiv.appendChild(newCharacter)
+  }
+
+  writtenBlock(elm, div) {
+    let block = `<div class="character-info">
+          <div class="name">Character Id: ${elm.id}</div>
+          <div class="name">Character Name: ${elm.name}</div>
+          <div class="occupation">Character Occupation: ${elm.occupation}</div>
+          <div class="cartoon">Is a Cartoon?: ${elm.cartoon}</div>
+          <div class="weapon">Character Weapon: ${elm.weapon}</div>
+        </div>`
+    div.innerHTML += block
+  }
+
+
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,132 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com/');
+
+updateList = () => {
+  charactersAPI
+    .getFullList()
+    .then(response => {
+      const { data } = response
+
+      let currentDiv = document.querySelector(".characters-container");
+
+      while (currentDiv.firstChild) {
+        currentDiv.removeChild(currentDiv.firstChild);
+      }
+
+      data.forEach(elm => {
+        charactersAPI.writtenBlock(elm, currentDiv)
+
+      }
+      )
+
+    })
+    .catch(err => console.log('error', err))
+
+}
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    updateList()
 
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
+    event.preventDefault()
+    const inputs = document.querySelector('.operation input')
+
+    charactersAPI
+      .getOneRegister(inputs.value)
+      .then(selection => {
+        let div = document.querySelectorAll(".operation")[1];
+        charactersAPI.writtenBlock(selection, div)
+      })
+
+
+
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    event.preventDefault()
+    const inputs = document.querySelector('.operation.delete input')
+
+    charactersAPI
+      .deleteOneRegister(inputs.value)
+      .then(() => document.getElementById('delete-one').classList.add("active"))
+      .catch(err => {
+        console.log(err)
+        document.getElementById('operation delete').classList.add("inactive")
+      })
 
   });
 
+  document.querySelector('#receive-character').onsubmit = e => {
+    e.preventDefault()
+
+    const characterId = document.querySelector('#receive-character input').value
+    const input = document.querySelectorAll('#edit-character-form input')
+
+    charactersAPI
+      .getOneRegister(characterId)
+      .then(response => {
+        fillEditForm(response.data)
+        document.querySelector('#receive-character').reset()
+      })
+      .catch(err => console.log('error', err))
+
+  }
+
+  const fillEditForm = characterInfo => {
+    const input = document.querySelectorAll('#edit-character-form input')
+    input[0].value = characterInfo.name
+    input[1].value = characterInfo.occupation
+    input[2].value = characterInfo.weapon
+    input[3].checked = characterInfo.cartoon
+    input[4].value = characterInfo.id           // oculto
+  }
+
+
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const inputs = document.querySelectorAll('#edit-character-form input')
+
+    const character = {
+      name: inputs[0].value,
+      occupation: inputs[1].value,
+      weapon: inputs[2].value,
+      cartoon: inputs[3].checked
+    }
+
+    const id = inputs[4].value
+
+    charactersAPI
+      .updateOneRegister(id, character)
+      .then(() => {
+        document.querySelector('#edit-character-form').reset()
+        updateList()
+      })
+      .catch(err => console.log('error', err))
 
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
 
+    event.preventDefault()
+    const inputs = document.querySelectorAll('#new-character-form input')
+
+    const character = {
+      name: inputs[0].value,
+      occupation: inputs[1].value,
+      weapon: inputs[2].value,
+      cartoon: inputs[3].checked
+    }
+
+    charactersAPI
+      .createOneRegister(character)
+      .then(() => {
+        document.querySelector('#new-character-form').reset()
+        updateList()
+      })
+      .catch(err => console.log('error', err))
   });
 });
