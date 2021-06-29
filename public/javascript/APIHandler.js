55af67d09cfcd49219d4dd9af2ce1d26cbadd7b7
Kikec82 <kikecolo2007@yahoo.es>
2021-06-29 23:51:47 +0200

    lab mnions postman

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,24 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.BASE_URL = 'https://minions-api.herokuapp.com/';
+    this.api = axios.create({
+      baseURL: baseUrl
+    })
   }
-
-  getFullList () {
-
-  }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
+  getFullList() {
+    return this.api.get('/characters')
 
   }
-
-  updateOneRegister () {
-
+  getOneRegister(id) {
+    return this.api.get(`/characters/${id}`)
   }
-
-  deleteOneRegister () {
-
+  createOneRegister(characterInfo) {
+    return this.api.post('/characters', characterInfo)
+  }
+  updateOneRegister(id, characterInfo) {
+    return this.api.put(`/characters/${id}`,characterInfo)
+  }
+  deleteOneRegister(id) {
+    return this.api.delete(`/characters/${id}`)
   }
 }
