345645cf897c57567c6540ed00637705311adb1c
javierceballosaltuna <javierceballosaltuna@gmail.com>
2021-06-30 23:45:36 +0200

    añadido delete y revisando fallos en create, pengin

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -42,40 +42,127 @@ window.addEventListener('load', () => {
     let singleID = document.querySelector('.operation input').value
     //tomamos el valor del ID introducido
 
-    charactersAPI
-      //lo pasamos como parametro de la funcion desplegar uno y añadimos sus datos
-      .displayOne(singleID)
-      .then(elm => {console.log(response);
+    function loadCharactersFromAPI() {
 
-        let character = `<div class="character-info">
+      charactersAPI
+        //lo pasamos como parametro de la funcion desplegar uno y añadimos sus datos
+        .displayOne(singleID)
+        .then(elm => {
+          console.log(elm);
 
-        <div class="name">Character Name: ${elm.name}</div>
-        <div class="occupation">Character Occupation: ${elm.occupation}</div>
-        <div class="cartoon">Is a Cartoon? ${elm.cartoon}</div>
-        <div class="weapon">Character Weapon: ${elm.weapon}</div>
+
+          //IMPORTANTE PONER ELM.DATA porque data es una key del objeto elm que  asu vez es otro objeto  y accedemos a su valor .name
+
+
+          let character = `<div class="character-info">
+
+          
+        <div class="name">Character Name: ${elm.data.name}</div>
+        <div class="occupation">Character Occupation: ${elm.data.occupation}</div>
+        <div class="cartoon">Is a Cartoon? ${elm.data.cartoon}</div>
+        <div class="weapon">Character Weapon: ${elm.data.weapon}</div>
 
       </div> `
 
-        document.querySelector('.characters-container').innerHTML = character
+          document.querySelector('.characters-container').innerHTML = character
 
-      })
-      .catch(err => console.log(err))
 
+        })
+        .catch(err => console.log(err))
+    }
+
+
+
+
+
+    loadCharactersFromAPI(singleID)
 
   })
-      //console.log(singleCharacter)
 
-      ;
+
+    //console.log(singleCharacter)
+
+    ;
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
 
-  });
+    let singleID = document.querySelector('.operation.delete input').value
+    //tomamos el valor del ID introducido
+
+    function removeCharactersFromAPI() {
+
+      charactersAPI
+        //lo pasamos como parametro de la funcion remove 
+        .deleteOneRegister(singleID,)//no metemos info porque queremos eliminar, en el caso de editar aquí si habrá que pasarle datos
+        .then(() => {
+          //METER EL COLOR DEK BOTON A VERDE
+
+        })
+        .catch(err => console.log(err))//PONER EL BOTON EN ROJO SI ALGO HA FALLADO
+    }
+
+    removeCharactersFromAPI(singleID)
+
+  })
+
+
+
+
+
+
+
+
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
+
+
+
+    //   document.querySelector('#getCharacterInfoForm').onsubmit = e => {
+
+    //   e.preventDefault()
+
+    //     Let singleID = document.querySelector('#edit-character-form input').value
+
+    //   apiHandler
+
+    //     .displayOne(singleID)
+    //     .then(response => {
+    //       fillCharacterEditForm(response.data)
+    //       document.querySelector('#edit-character-form').reset()
+    //     })
+    //     .catch(err => console.log(err))
+    // }
+
+
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.querySelector('#new-character-form #send-data').addEventListener('submit', function (event) {
 
-  });
+
+
+    let campo = document.querySelectorAll('#new-character-form input')
+
+ let character = campo.forEach(elm=> elm.value)
+  
+
+    
+
+
+
+    charactersAPI
+
+      .createOneRegister(character)
+      //método con el que añadimos un registro nuevo pasándole el argumento del nuevo character
+      //genera un request de tipo post que lke envia los datos a la api mediante axios
+      .then(() => {
+        loadCharactersFromAPI()
+        document.querySelector('#new-character-form').reset()
+      })
+      .catch(err => console.log(err))
+  })
+
+
+
+
 });
